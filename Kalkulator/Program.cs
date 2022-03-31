d3601e359b54f152c738b8af7bc75db85a9c7fc3
Alicja <alichr07@gmail.com>
2022-03-31 21:06:51 +0200

    Dodaj pliki projektów.

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wykonanie działanie na 2 liczbach
+
+            Console.WriteLine("Podaj 1. liczbę: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj znak działania: ");
+            string z = Console.ReadLine();
+            Console.WriteLine("Podaj 2. liczbę: ");
+            double b = double.Parse(Console.ReadLine());
+
+            if (z == "+")
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (z == "-")
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (z == "*")
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (z == "/")
+            {
+                Console.WriteLine(a / b);
+            }
+            else
+                Console.WriteLine("Nie ma takiego działania.");
+            Console.ReadKey();
+        }
+    }
+}
